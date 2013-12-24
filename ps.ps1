18e64e087c56266324998fbc151f1bb594ba4e9a
litao <litao.git@gmail.com>
2013-12-24 20:33:36 +0800

    cleanup codes

--- ps.ps1
+++ ps.ps1
@@ -5,75 +5,72 @@
 
 # plz change the end of this file for your usages
 
+# log file
+$logfile = "log\ps.log"
+$logpath =  Split-Path $logfile
+if (! (Test-Path $logpath) ) { mkdir $logpath } 
+
 $timestr=get-date -uformat "%Y-%m-%d-%H-%M-%S"
 
-function start_and_check_without_argment( ) {
+function start_and_check_without_argment {
+  $processArray = New-Object string[] ( $args.count)
+  $argsArray = New-Object string[] ( $args.count)
+  $pidArray = New-Object string[] ( $args.count )
 
-	$processArray = New-Object string[] ( $args.count)
-	$argsArray = New-Object string[] ( $args.count)
-	$pidArray = New-Object string[] ( $args.count )
+  #parse cmd and run them
+  for($j=0;$j -lt $args.count; $j++){      
+    $processArray[$j] = $args[$j]  
+    $pidArray[$j]  = [Diagnostics.Process]::Start($processArray[$j]).ID
+    Write-Host $timestr "started process: " + $processArray[$j] + " PID: " +  $pidArray[$j]
+    } 
 
-	#parse cmd and run them
-	for($j=0;$j -lt $args.count; $j++){      
-	    $processArray[$j] = $args[$j]  
-		$pidArray[$j]  = [Diagnostics.Process]::Start($processArray[$j]).ID
-	    Write-Host $timestr "started process: " + $processArray[$j] + " PID: " +  $pidArray[$j]
-	    } 
-    
+  #check whether pids is exists, or run them
+  while(1){    
+    for($i=0;$i -lt $args.count; $i++) {    
+      if(!(get-process -id $pidArray[$i] -ErrorAction silentlycontinue)) {
+        $pidArray[$i] = [Diagnostics.Process]::Start($processArray[$i]).ID
+        Write-Host $timestr "auto restarted process: " + $processArray[$i]  + " PID: " +  $pidArray[$i] 
+      }
+    }
 
-    #check whether pids is exists, or run them
-	while(1){    
-        for($i=0;$i -lt $args.count; $i++) {    
-	    	if(  get-process -id $pidArray[$i] -ErrorAction silentlycontinue){ }
-			else{
-				$pidArray[$i] = [Diagnostics.Process]::Start($processArray[$i]).ID
-	        	Write-Host $timestr "auto restarted process: " + $processArray[$i]  + " PID: " +  $pidArray[$i] 
-			}
-        }
-  
     # waits for 10 seconds    
-	sleep 10
-	}
+    sleep 10
+  }
 }
 
-function start_and_check_with_argment( ) {
-	
-	#check argments 
-	if( ( $args.count -lt 2 ) -or ( $args.count %2 -gt  0)){
-	"Please check your files,and run again."
-	$args.count
-	exit
-	}
+function start_and_check_with_argment {  
+  #check argments 
+  if( ( $args.count -lt 2 ) -or ( $args.count %2 -gt  0)) {
+  "Please check your argments, and run again."
+  exit
+  }
 
+  $processArray = New-Object string[] ( $args.count/2)
+  $argsArray = New-Object string[] ( $args.count/2)
+  $pidArray = New-Object string[] ( $args.count/2 )
+  $j = 0
 
-	$processArray = New-Object string[] ( $args.count/2)
-	$argsArray = New-Object string[] ( $args.count/2)
-	$pidArray = New-Object string[] ( $args.count/2 )
-	$j = 0
-
-	#parse cmd and argments and run then
-	for($i=0;$i -lt $args.count; $i=$i+2){      
-	    $processArray[$j] = $args[$i]
-	    $argsArray[$j] = $args[$i+1]     
-		$pidArray[$j]  = [Diagnostics.Process]::Start($processArray[$j] , $argsArray[$j]).ID
-	    Write-Host $timestr "started process: " + $processArray[$j] + $argsArray[$j] + " PID: " +  $pidArray[$j]        
-	    $j++        
-	    } 
-    
-
-    #check whether pids is exists, or run cmd with argment
-	while(1){    
-        for($i=0;$i -lt $j; $i=$i+1) {    
-	    	if(  get-process -id $pidArray[$i] -ErrorAction silentlycontinue){ }
-			else{
-				$pidArray[$i] = [Diagnostics.Process]::Start($processArray[$i] , $argsArray[$i]).ID
-	        	Write-Host $timestr "auto restarted process: " + $processArray[$i] + $argsArray[$i] + " PID: " +  $pidArray[$i] 
-			}
-        }
+  #parse cmd and argments and run then
+  for($i=0;$i -lt $args.count; $i=$i+2) {      
+    $processArray[$j] = $args[$i]
+    $argsArray[$j] = $args[$i+1]     
+    $pidArray[$j]  = [Diagnostics.Process]::Start($processArray[$j] , $argsArray[$j]).ID
+    Write-Host $timestr "started process: " + $processArray[$j] + $argsArray[$j] + " PID: " +  $pidArray[$j]        
+    $j++        
+  } 
+  
+  #check whether pids is exists, or run cmd with argment
+  while(1){
+    for($i=0;$i -lt $j; $i=$i+1) {    
+      if( !(get-process -id $pidArray[$i] -ErrorAction silentlycontinue)){ 
+        $pidArray[$i] = [Diagnostics.Process]::Start($processArray[$i] , $argsArray[$i]).ID
+        Write-Host $timestr "auto restarted process: " + $processArray[$i] + $argsArray[$i] + " PID: " +  $pidArray[$i] 
+      }
+    }
   
     # waits for 10 seconds    
-	sleep 10
-	}
+    sleep 10
+  }
 }
 
 
@@ -81,6 +78,4 @@ function start_and_check_with_argment( ) {
 
 #start_and_check_with_argment "notepad"  "d:\1.txt" "notepad" "d:\3.txt" "notepad"  "d:\5.txt" "notepad"  "d:\9.txt"
 
-#start_and_check_without_argment "notepad" "calc" "cmd"
-
-
+start_and_check_without_argment "notepad" "calc" "cmd"
